875007a9e95195ad795c5d45c38a5c8ff6c59558
Matthew Brown <brownm18@rpi.edu>
2019-03-11 16:57:22 -0400

    Upload

---
+++ Assets/Scripts/Appearing_Images.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Appearing_Images : MonoBehaviour {
+
+    public RawImage img0;               // Image for scene 1a
+    public float img0_sec;              // Amount of time Image 0 is displayed
+    public RawImage img1;               // Image for scene 1b
+    public float img1_sec;              // Amount of time Image 1 is displayed
+    public RawImage img2;               // Image for scene 1c
+    public RawImage img3;               // Image for scene 1c
+    public float img2_3_sec;            // Amount of time Image 2 and 3 is displayed
+    public RawImage img4;               // Image for scene 3
+    public RawImage img5;               // Image for scene 3
+    private List<float> step = new List<float>(); // Incromentation of image's location
+    private List<float> step_degree = new List<float>(); // Incromentation of image's degree
+    private List<RawImage> images = new List<RawImage>(); // List of Images
+
+    public AudioSource Scene1_a;        // Audio for Scene1_a
+    public AudioSource Scene1_b;        // Audio for Scene1_b
+    public AudioSource Scene1_c;        // Audio for Scene1_c
+    public AudioSource Scene3_a;        // Audio for Scene3_a
+    public AudioSource Scene5_a;        // Audio for Scene5_a
+    public AudioSource Scene5_b;        // Audio for Scene5_b
+    public AudioSource Scene6_b;        // Audio for Scene6_b
+
+    public GameObject Head;
+
+    public VideoPlayer Maya;
+    public VideoPlayer LipSyncPro;
+
+
+    // Use this for initialization
+    void Start () {
+        Head.active = false;
+        Maya.enabled = false;
+        LipSyncPro.enabled = false;
+
+        images.Add(img0);               // Add Image 0 to images
+        images.Add(img1);               // Add Image 1 to images
+        images.Add(img2);               // Add Image 2 to images
+
+        img3.enabled = false;           // Make image invisiible
+        img4.enabled = false;           // Make image invisiible
+        img5.enabled = false;           // Make image invisiible
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            images[i].enabled = false;  // Make image invisiible
+        }
+
+        step.Add(1 / (img0_sec * 2));   // Sets step of each image
+        step.Add(1 / (img1_sec * 2));   // Sets step of each image
+        step.Add(1 / (img2_3_sec * 2)); // Sets step of each image
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            float num_steps = 40 / step[i];  // Number of steps for each image
+            step_degree.Add(90 / num_steps); // Set step_degree for each image
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            if (images[i].enabled == true)  // If the Image is visible 
+            {
+                Vector3 old_loc = images[i].transform.position; // Old location of image
+
+                if (old_loc[0] > 5)         // Image is on the right side of the screen
+                {
+                    Vector3 new_loc = new Vector3(old_loc[0] - step[i], old_loc[1], old_loc[2] + step[i]);
+                    images[i].transform.position = new_loc;     // Change location
+                }
+                else if (old_loc[0] > -35)  //Image is on the left side of the screen
+                {
+                    Vector3 new_loc = new Vector3(old_loc[0] - step[i], old_loc[1], old_loc[2] - step[i]);
+                    images[i].transform.position = new_loc;     // Change location
+                }
+                else
+                {
+                    images[i].enabled = false;  // Image has moved across screen
+                    if (i == 2)                 // Image 2
+                    {
+                        img3.enabled = false;   // Make image 3 invisible
+                    }
+                }
+
+                Vector3 old_rot = images[i].transform.eulerAngles;  // Old angle of image
+                Vector3 new_rot = new Vector3(old_rot[0], old_rot[1] - step_degree[i], old_rot[2]);
+                images[i].transform.eulerAngles = new_rot;          // Change angle
+            }
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            if (images[0].enabled == false)
+            {
+                images[0].transform.position = new Vector3(45, 20, 10);
+                images[0].transform.eulerAngles = new Vector3(-63, 90, 0);
+                images[0].enabled = true;
+            }
+            else
+            {
+                images[0].enabled = false;
+            }
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            if (images[1].enabled == false)
+            {
+                images[1].transform.position = new Vector3(45, 20, 10);
+                images[1].transform.eulerAngles = new Vector3(-63, 90, 0);
+                images[1].enabled = true;
+            }
+            else
+            {
+                images[1].enabled = false;
+            }
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            if (images[2].enabled == false)
+            {
+                images[2].transform.position = new Vector3(45, 20, 10);
+                images[2].transform.eulerAngles = new Vector3(-63, 90, 0);
+                images[2].enabled = true;
+                img3.enabled = true;
+            }
+            else
+            {
+                images[2].enabled = false;
+                img3.enabled = false;
+            }
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+            if (img4.enabled == false)
+            {
+                img4.enabled = true;
+                img5.enabled = true;
+            }
+            else
+            {
+                img4.enabled = false;
+                img5.enabled = false;
+            }
+        }
+        else if (Input.GetKeyDown("z"))
+        {
+            Scene1_a.Play(0);
+        }
+        else if (Input.GetKeyDown("x"))
+        {
+            Scene1_b.Play(0);
+        }
+        else if (Input.GetKeyDown("c"))
+        {
+            Scene1_c.Play(0);
+        }
+        else if (Input.GetKeyDown("v"))
+        {
+            Scene3_a.Play(0);
+        }
+        else if (Input.GetKeyDown("b"))
+        {
+            Scene5_a.Play(0);
+        }
+        else if (Input.GetKeyDown("n"))
+        {
+            Scene5_b.Play(0);
+        }
+        else if (Input.GetKeyDown("m"))
+        {
+            if (Maya.enabled == true)
+            {
+                Maya.enabled = false;
+            }
+            else
+            {
+                Maya.enabled = true;
+                Maya.Play();
+            }
+        }
+        else if (Input.GetKeyDown("7"))
+        {
+            if (LipSyncPro.enabled == true)
+            {
+                LipSyncPro.enabled = false;
+            }
+            else
+            {
+                LipSyncPro.enabled = true;
+                LipSyncPro.Play();
+            }
+        }
+        else if (Input.GetKeyDown("6"))
+        {
+            if (Head.active == true)
+            {
+                Head.active = false;
+            }
+            else
+            {
+                Head.active = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FishToSphereTest.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class FishToSphereTest : MonoBehaviour {
+
+    Vector3[] verts;
+    Vector2[] uvs;
+    int[] triangles;
+    Mesh mesh;
+
+    float PID2 = Mathf.PI / 2;
+    int NX = 100;
+    int NY = 60;
+
+	// Use this for initialization
+	void Start () {
+        verts = new Vector3[6000];
+        uvs = new Vector2[6000];
+        Generate();
+       
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Generate(){
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+
+
+        //string file = System.IO.File.ReadAllText(Application.dataPath + "/Resources/standard_16x9.data");
+
+        string file = Resources.Load<TextAsset>("standard_16x9").text;
+        string[] lines = file.Split('\n');
+        // 6003 lines long, 2 intro, 6000 meaningful, one blank
+        for (int i = 0; i < 6000; i++)
+        {
+            string[] lineData = lines[i + 2].Trim().Split();
+            verts[i].x = float.Parse(lineData[0]);
+            verts[i].y = float.Parse(lineData[1]);
+            verts[i].z = 0f;
+            uvs[i].x = float.Parse(lineData[2]);
+            uvs[i].y = float.Parse(lineData[3]);
+            //yield return null;
+        }
+        mesh.vertices = verts;
+        mesh.uv = uvs;
+
+        int xSize = 99;
+        int ySize = 59;
+
+        triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.black;
+    //    for (int i = 0; i < verts.Length; i++)
+    //    {
+    //        Gizmos.DrawSphere(verts[i], 0.005f);
+    //    }
+    //}
+
+
+
+    //int i, j;
+    //float x, y, u, v;
+    //Vector3 p;
+    //float theta, phi, r;
+
+    //for (j = 0; j <= NY; j++)
+    //{
+    //    for (i = 0; i <= NX; i++)
+    //    {
+    //        x = -1 + 2 * i / (float)NX;
+    //        y = -1 + 2 * j / (float)NY;
+    //        theta = PID2 + x * PID2;
+    //        phi = y * PID2;
+    //        p.x = Mathf.Cos(phi) * Mathf.Cos(theta);
+    //        p.y = Mathf.Cos(phi) * Mathf.Sin(theta);
+    //        p.z = Mathf.Sin(phi);
+    //        theta = Mathf.Atan2(p.z, p.x);
+    //        phi = Mathf.Atan2(Mathf.Sqrt(p.x * p.x + p.z * p.z), p.y);
+    //        r = phi / PID2;
+    //        u = (1 + r * Mathf.Cos(theta)) / 2;
+    //        v = (1 + r * Mathf.Sin(theta)) / 2;
+    //        //print(string.Format("{0} {1} {2} {3} 1\n", x, y, u, v));
+    //    }
+    //}
+}
